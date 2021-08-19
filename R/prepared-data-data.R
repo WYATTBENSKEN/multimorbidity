79870db5e76179d0cf6d3e3df5fcd8f049183208
Wyatt Bensken <wpb27@case.edu>
2021-08-19 14:04:36 -0400

    Adding a dataset of prepared data, to be used in examples of the functions.

---
+++ R/prepared-data-data.R
@@ -0,0 +1,11 @@
+#' Prepared dataset
+#'
+#' A dataset which has been prepared using the prepare_data function in this package.
+#'
+#' @source Built using the packages in this code.
+#'
+#' @docType data
+#'
+#' @usage data(prepared_data)
+#'
+"prepared_data"

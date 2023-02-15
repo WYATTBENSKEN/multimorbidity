5a2d1df6819cc7a26861de37adb3f435c20a4037
WYATTBENSKEN <wpb27@case.edu>
2023-02-15 08:49:57 -0500

    Fixing CRAN submission notes.

--- R/util-checks.R
+++ R/util-checks.R
@@ -1,5 +1,5 @@
 check_dx <- function(dx){
-  if (class(dx) != "character") {
+  if (is.character(dx) != TRUE) {
     warning("Your diagnoses were not character variable(s) and have been converted to such. To avoid this warning, please convert diagnoses to characters before this step.")
     dx <- as.character(dx)
   }
--- cran-comments.md
+++ cran-comments.md
@@ -1,3 +1,6 @@
+## This is a resubmission
+* To fix the notes that came up, however we do have non-ASCII characters so that note will remain.
+
 ## Test environments
 * local OS X install, R 4.1.3
 * win-builder (devel and release)
